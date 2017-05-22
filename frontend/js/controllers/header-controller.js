ce851817ad23b80de6e91c8fadfaeafe2d4efb91
snehalthakare <snehal.thakare@wohlig.com>
2017-05-22 19:20:08 +0530

    contact-modal

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -1,7 +1,17 @@
-myApp.controller('headerCtrl', function ($scope, TemplateService) {
+myApp.controller('headerCtrl', function ($scope, TemplateService, $uibModal) {
     $scope.template = TemplateService;
     $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
         $(window).scrollTop(0);
     });
     $.fancybox.close(true);
+    $scope.contactopen = function () {
+        console.log("clla");
+        $uibModal.open({
+            animation: true,
+            templateUrl: 'views/modal/contact-modal.html',
+            scope: $scope,
+            size: 'lg',
+
+        });
+    };
 });
