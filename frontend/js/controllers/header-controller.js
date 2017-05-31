870aa11dd4251339743c115e7d2824c4ac1b4217
snehalthakare <snehal.thakare@wohlig.com>
2017-05-31 13:17:32 +0530

    ask-expert modal and small changes

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -15,10 +15,14 @@ myApp.controller('headerCtrl', function ($scope, TemplateService, $uibModal) {
         });
     };
     $(window).scroll(function () {
-        if ($(document).scrollTop() > 100) {
+        if ($(document).scrollTop() > 200) {
             $(".header-border").css("opacity", '0');
+            $(".img-width-change").css("width", '10%');
+            $(".navbar-color-change").css("background", 'rgba(0, 0, 0, 0.54)');
         } else {
             $(".header-border").css("opacity", '1');
+            $(".navbar-color-change").css("background", 'transparent');
+            $(".img-width-change").css("width", '25%');
         }
     });
 });
--- frontend/js/controllers/minutestips.js
+++ frontend/js/controllers/minutestips.js
@@ -1,4 +1,4 @@
- myApp.controller('MinutestipsCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+ myApp.controller('MinutestipsCtrl', function ($scope, TemplateService, NavigationService, $timeout, $uibModal) {
      $scope.template = TemplateService.getHTML("content/minutestips.html");
      TemplateService.title = "Minutestips"; //This is the Title of the Website
      $scope.navigation = NavigationService.getNavigation();
@@ -6,5 +6,15 @@
      $scope.submitForm = function (data) {
          console.log(data);
          $scope.formSubmitted = true;
+     };
+     $scope.askexpertopen = function () {
+         console.log("clla");
+         $uibModal.open({
+             animation: true,
+             templateUrl: 'views/modal/ask-expert.html',
+             scope: $scope,
+             windowClass: 'ask-modal'
+
+         });
      };
  })
