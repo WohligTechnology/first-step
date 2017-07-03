a985440db946028640faf781a93b138503d3ed80
Sohan Honakeri <shn619@gmail.com>
2017-07-04 01:07:41 +0530

    web

--- backend/js/app.js
+++ backend/js/app.js
@@ -89,7 +89,7 @@ myApp.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locat
             controller: 'JagzCtrl'
         });
 
-    $urlRouterProvider.otherwise("/dashboard");
+    $urlRouterProvider.otherwise("/Userweb");
     $locationProvider.html5Mode(isproduction);
 });
 
