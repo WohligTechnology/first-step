69511cd5306c97bc80a4f54060fe6cb085cf1dea
System Administrator <root@Harshs-Mac-mini-3.local>
2017-08-03 12:04:14 +0530

    partnerspage

--- frontend/js/filters/filter.js
+++ frontend/js/filters/filter.js
@@ -1,19 +1,19 @@
 // JavaScript Document
 myApp.filter('myFilter', function () {
-        // In the return function, we must pass in a single parameter which will be the data we will work on.
-        // We have the ability to support multiple other parameters that can be passed into the filter optionally
-        return function (input, optional1, optional2) {
+    // In the return function, we must pass in a single parameter which will be the data we will work on.
+    // We have the ability to support multiple other parameters that can be passed into the filter optionally
+    return function (input, optional1, optional2) {
 
-            var output;
+        var output;
 
-            // Do filter work here
-            return output;
-        };
+        // Do filter work here
+        return output;
+    };
 
-    })
+});
 
-    .filter('safeHtml', function ($sce) {
-        return function (val) {
-            return $sce.trustAsHtml(val);
-        };
-    });
+myApp.filter('safeHtml', function ($sce) {
+    return function (val) {
+        return $sce.trustAsHtml(val);
+    };
+});
