3e48b33f4e2df52c47ab98f2a8c498dbfd82dd91
Srujana <srujana.vemula@wohlig.com>
2018-08-28 17:30:07 +0530

    digitial

--- api/services/DigitalUser.js
+++ api/services/DigitalUser.js
@@ -28,7 +28,7 @@ schema.plugin(deepPopulate, {
 // schema.plugin(uniqueValidator);
 schema.plugin(timestamps);
 module.exports = mongoose.model('DigitalUser', schema);
-
+mongoose.model('DigitalUser').collection.drop();
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema, "testGiven", "testGiven"));
 var model = {
 
