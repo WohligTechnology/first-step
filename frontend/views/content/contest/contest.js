c6c21d438524bd9aaaf2fc202a8145e9be9f00b6
Srujana <srujana.vemula@wohlig.com>
2018-10-01 13:43:15 +0530

    changed real states to real estate in 1 minute tips

--- frontend/views/content/contest/contest.js
+++ frontend/views/content/contest/contest.js
@@ -1,24 +1,24 @@
-
 myApp.controller('ContestCtrl', function ($scope, TemplateService, NavigationService, $timeout, toastr, $http) {
     $scope.template = TemplateService.getHTML("content/contest/contest.html");
     TemplateService.title = "Contest"; //This is the Title of the Website
     $scope.navigation = NavigationService.getNavigation();
     TemplateService.cssMain = "header-icons";
     $scope.season4 = [{
-        imageUrl: "ZOkWJZgERNE",
-        videoUrl: "ZOkWJZgERNE",
-        episodeno: "Tip No. 1",
-        searchName: "Debts Assets",
-    }, {
-        imageUrl: "QHkTqmexZjg",
-        videoUrl: "QHkTqmexZjg",
-        episodeno: "Tip No. 2",
-        searchName: "Equity",
-    },
-    {
-        imageUrl: "vUbwfD_G9Jg",
-        videoUrl: "vUbwfD_G9Jg",
-        episodeno: "Tip No. 3",
-        searchName: "Real States",
-    }];
+            imageUrl: "ZOkWJZgERNE",
+            videoUrl: "ZOkWJZgERNE",
+            episodeno: "Tip No. 1",
+            searchName: "Debts Assets",
+        }, {
+            imageUrl: "QHkTqmexZjg",
+            videoUrl: "QHkTqmexZjg",
+            episodeno: "Tip No. 2",
+            searchName: "Equity",
+        },
+        {
+            imageUrl: "vUbwfD_G9Jg",
+            videoUrl: "vUbwfD_G9Jg",
+            episodeno: "Tip No. 3",
+            searchName: "Real Estate",
+        }
+    ];
 })
