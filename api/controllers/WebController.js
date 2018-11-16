b4309285cd6de09a38fbcab882813d255b188f5e
Srujana <srujana.vemula@wohlig.com>
2018-11-16 18:56:17 +0530

    november blogs added

--- api/controllers/WebController.js
+++ api/controllers/WebController.js
@@ -310,6 +310,27 @@ module.exports = {
                     keywords: "Retirement,retirement fund, saving,insurance policy,early retirement,payment of taxes,"
 
                 });
+            } else if (req.params.name == "lock-unlock-biometric-details-of-aadhar-card") {
+                res.metaView({
+                    title: "Pehla Kadam | Lock and unlock online the biometric details of your Aadhar Card",
+                    description: "",
+                    keywords: "lock biometrics online, unlock biometrics online, aadhar card, proof of identity, verification, obtain sim card, aadhar card school admission, retinal scan aadhar card, finger print aadhar card, biometric format aadhar card, disable biometric lock, registered mobile number, OTP aadhar card, uidai, security code aadhar cardPAN card"
+
+                });
+            } else if (req.params.name == "link-PAN-AAdhar-to-process-insurance-claims") {
+                res.metaView({
+                    title: "Pehla Kadam | Link your PAN or AAdhar to process insurance claims",
+                    description: "",
+                    keywords: "Form 60, Form 61, IRDA, PAN card, Aadhar card, insurance policy, insurance policies, insurance companies, insurance company, link Aadhar card, link PAN card, link to insurance, insurance claims"
+
+                });
+            } else if (req.params.name == "know-bank-balance-using-smart-phone-without-internet") {
+                res.metaView({
+                    title: "Pehla Kadam | Know your bank balance using your smart phone without internet",
+                    description: "",
+                    keywords: "bank balance offline, bank balance without internet, internet banking offline, missed call alert, dial *99#, NPCI, IFSC code, MMID, dual factor authentication"
+
+                });
             }
         }
     },
