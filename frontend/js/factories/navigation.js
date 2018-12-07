f902d0348145dd8d7ee2135526a867df8374c95e
Chintan Shah <chintan@wohlig.com>
2018-12-07 20:18:24 +0530

    ok

--- frontend/js/factories/navigation.js
+++ frontend/js/factories/navigation.js
@@ -1,65 +1,66 @@
-myApp.factory('NavigationService', function ($http) {
-    var navigation = [
-        //     {
-        //     name: "Home",
-        //     classis: "active",
-        //     anchor: "home",
-        //     subnav: [{
-        //         name: "Subnav1",
-        //         classis: "active",
-        //         anchor: "home"
-        //     }]
-        // }, {
-        //     name: "Links",
-        //     classis: "active",
-        //     anchor: "links",
-        //     subnav: []
-        // }
-    ];
+myApp.factory("NavigationService", function($http) {
+  var navigation = [
+    //     {
+    //     name: "Home",
+    //     classis: "active",
+    //     anchor: "home",
+    //     subnav: [{
+    //         name: "Subnav1",
+    //         classis: "active",
+    //         anchor: "home"
+    //     }]
+    // }, {
+    //     name: "Links",
+    //     classis: "active",
+    //     anchor: "links",
+    //     subnav: []
+    // }
+  ];
 
-    return {
-        getNavigation: function () {
-            return navigation;
-        },
+  return {
+    getNavigation: function() {
+      return navigation;
+    },
 
-        apiCallWithData: function (url, formData, callback) {
-            $http.post(adminurl + url, formData).then(function (data) {
-                data = data.data;
-                callback(data);
+    apiCallWithData: function(url, formData, callback) {
+      $http.post(adminurl + url, formData).then(function(data) {
+        data = data.data;
+        callback(data);
+      });
+    },
 
-            });
-        },
-
-        apiCallWithoutData: function (url, callback) {
-            $http.post(adminurl + url).then(function (data) {
-                data = data.data;
-                callback(data);
-            });
-        },
-        getBlogs: function (callback) {
-            $http.post(adminurl + "Blog/getBlogs").then(function (data) {
-                data = data.data;
-                callback(data);
-            });
-        },
-        getselectedBlog: function (data, callback) {
-            $http.post(adminurl + "Blog/search", data).then(function (data) {
-                data = data.data;
-                callback(data.data.results[0]);
-            });
-        },
-        getSeo: function (data, callback) {
-            $http.post(adminurl + "SeoBackend/getOneSEO", {
-                name: data
-            }, function (data) {
-                callback(data);
-            })
-        },
-        getHomeScreenBlogs: function (callback) {
-            $http.post(adminurl + "Blog/getHomeScreenBlogs").then(function (data) {
-                data = data.data;
-                callback(data);
-            });
-        }
-    };
+    apiCallWithoutData: function(url, callback) {
+      $http.post(adminurl + url).then(function(data) {
+        data = data.data;
+        callback(data);
+      });
+    },
+    getBlogs: function(callback) {
+      $http.post(adminurl + "Blog/getBlogs").then(function(data) {
+        data = data.data;
+        callback(data);
+      });
+    },
+    getselectedBlog: function(data, callback) {
+      $http.post(adminurl + "Blog/search", data).then(function(data) {
+        data = data.data;
+        callback(data.data.results[0]);
+      });
+    },
+    getSeo: function(data, callback) {
+      $http
+        .post(adminurl + "SeoBackend/getOneSEO", {
+          name: data
+        })
+        .then(function(data) {
+          callback(data);
+        });
+    },
+    getHomeScreenBlogs: function(callback) {
+      $http.post(adminurl + "Blog/getHomeScreenBlogs").then(function(data) {
+        data = data.data;
+        callback(data);
+      });
+    }
+  };
 });
